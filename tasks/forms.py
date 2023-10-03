6951ef02e4262c5688c8ffd32b7739f0f81895d7
Hjriebe <hjriebe@alaska.edu>
2023-10-02 17:03:22 -0800

    all good?

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
 class NewTaskForm(forms.form):
-    task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10, initial=0)
+    task = forms.CharField(label="New Task", min_length=5)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=3, initial=1)
+
+    def clean(self):
+        super(NewTaskForm, self.clean())
+
+        t = self.cleaned_data('task')
+        p = self.cleaned_data('priority')
+
+        if p >= 4 and len(t) < 10:
+            self._errors['task'] = self.error_class([
+                'Minimum 10 chars needed when priority >= 4'
+            ])
+        return self.cleaned_data
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import NewTaskForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 tasks = ['eat', 'sleep', 'pray'
 ]
@@ -14,3 +16,67 @@ def add(request):
 
 def add(request):
     return render(request, "tasks/add.html", {"form": NewTaskForm()})
+
+def add(request):
+    if request.method == "POST":
+        task = request.POST["task"]
+        priority = request.POST["priority"]
+        tasks.append(task)
+        return render(request, 'tasks/index.html', {'tasks': tasks})
+    return render(request, "tasks/add.html", {"form": NewTaskForm()})
+    
+def add(request):
+    if request.method == "POST":
+        task = request.POST["task"]
+        priority = request.POST["priority"]
+        tasks.append(task)
+        #return HttpResponseRedirect(reverse('tasks:index'))
+        return redirect('tasks:index') # similar functionality to previous line
+    else:
+        return render(request, "tasks/add.html", {"form": NewTaskForm()})
+    
+def add(request):
+    if request.method == "POST":
+        f = NewTaskForm(request.POST)
+        if f.is_valid():
+            task = request.POST["task"]
+            priority = request.POST["priority"]
+            tasks.append(task)
+            return redirect('tasks:index')
+        else:
+            return render(request, 'tasks/add.html', {"from": f})
+
+    else:
+        return render(request, "tasks/add.html", {"form": NewTaskForm()})
+    
+def add(request):
+    if request.method == "POST":
+        f = NewTaskForm(request.POST)
+        if f.is_valid():
+            task = f.cleaned_data["task"]
+            priority = f.cleaned_data["priority"]
+            tasks.append(task)
+            return redirect('tasks:index')
+        else:
+            return render(request, 'tasks/add.html', {"from": f})
+    else:
+        return render(request, "tasks/add.html", {"form": NewTaskForm()})
+
+def add(request):
+    if request.method == "POST":
+        f = NewTaskForm(request.POST)
+        if f.is_valid():
+            task = f.cleaned_data["task"]
+            priority = f.cleaned_data["priority"]
+            if task not in tasks:
+                tasks.append(task)
+                return redirect('tasks:index')
+            else: 
+                return render (request, 'tasks/add.html', {
+                    'form': f,
+                    'errormessage': "Duplicate tasks aren't allowed"
+                })
+        else:
+            return render(request, 'tasks/add.html', {"from": f})
+    else:
+        return render(request, "tasks/add.html", {"form": NewTaskForm()})
